3a3f0ba3babd352eb6ff436a162b6ac25b41846b
j2469li <j2469li@uwaterloo.ca>
2022-10-03 12:14:06 -0400

    Initialization

---
+++ a1/a1-basic/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+import javafx.application.Application
+import javafx.stage.Stage
+
+class Main : Application()  {
+    override fun start(primaryStage: Stage) {
+        primaryStage.show()
+    }
+}
